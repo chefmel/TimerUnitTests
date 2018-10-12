96596369f2f210beed4fd0263966ea4bf3cdc9ba
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-10-12 14:05:21 -0500

    Add project files.

---
+++ TimerUnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimerUnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
